546b3b85e4da9fb43ff483403944b72bab3e428c
fabrixiop26 <fabriciopuche@hotmail.com>
2022-03-19 15:12:56 -0500

    feat: added working carousel

--- lib/screens/pet_profile.dart
+++ lib/screens/pet_profile.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -27,12 +28,9 @@ class PetProfileWindow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-        padding: const EdgeInsets.only(
-            top: 16.0, left: 16.0, right: 16.0, bottom: 10.0),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: const Color(0xffE2E2EC)),
+    return Card(
+        color: Colors.transparent,
+        elevation: 0,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
@@ -86,9 +84,74 @@ class PetProfileWindow extends StatelessWidget {
                 value: "Yes",
               ))
             ]),
-            Center(child: Icon(Icons.more_horiz))
           ],
         ));
+  }
+}
+
+class CardExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: Center(
+        child: Text("ABC"),
+      ),
+    );
+  }
+}
+
+class Carousel extends StatefulWidget {
+  @override
+  final List<Widget> widgets;
+  Carousel(this.widgets);
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _CarouselState(widgets);
+  }
+}
+
+class _CarouselState extends State<Carousel> {
+  @override
+  int _current = 0;
+  final List<Widget> children;
+  _CarouselState(this.children);
+  final CarouselController _controller = CarouselController();
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(children: [
+      Expanded(
+          child: CarouselSlider(
+              items: children,
+              options: CarouselOptions(
+                  height: 200,
+                  viewportFraction: 1,
+                  enableInfiniteScroll: false,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  }))),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: children.asMap().entries.map((entry) {
+          return GestureDetector(
+            onTap: () => _controller.animateToPage(entry.key),
+            child: Container(
+              width: 4.0,
+              height: 4.0,
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 2.0),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: (Theme.of(context).brightness == Brightness.dark
+                          ? Colors.white
+                          : Colors.black)
+                      .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+            ),
+          );
+        }).toList(),
+      )
+    ]);
   }
 }
 
@@ -125,7 +188,14 @@ class PetProfile extends StatelessWidget {
                         color: Colors.black)),
                 IconButton(icon: Icon(Icons.edit), onPressed: null)
               ]),
-              PetProfileWindow()
+              Container(
+                  height: 250,
+                  padding: const EdgeInsets.only(
+                      top: 16.0, left: 16.0, right: 16.0, bottom: 10.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xffE2E2EC)),
+                  child: Carousel([PetProfileWindow(), PetProfileWindow()]))
             ])),
       ],
     );
