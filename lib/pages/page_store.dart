a2b3dedaa0896949a85a38ece97518f4c34c81d2
Jaime Sierra <jaimesierra@Jaimes-MacBook-Air.local>
2022-03-21 00:43:32 -0500

    style: styling products cards and separating components in widgets

--- lib/pages/page_store.dart
+++ lib/pages/page_store.dart
@@ -14,14 +14,14 @@ class _page_storeState extends State<page_store> {
   List<Product> entries = <Product>[];
   @override
   void initState() {
-    entries.add(Product(0, "https://pbs.twimg.com/media/ESc5JEQUUAAUJ1W.jpg",
-        "Coringa", "DavidSolanoSA", "5"));
-    entries.add(Product(0, "https://pbs.twimg.com/media/ESc5JEQUUAAUJ1W.jpg",
-        "Arroz", "MercadoLibre", "9"));
-        entries.add(Product(0, "https://pbs.twimg.com/media/ESc5JEQUUAAUJ1W.jpg",
-        "Arroz", "MercadoLibre", "9"));
-        entries.add(Product(0, "https://pbs.twimg.com/media/ESc5JEQUUAAUJ1W.jpg",
-        "Arroz", "MercadoLibre", "9"));
+    entries.add(Product(0, "https://cdn.discordapp.com/attachments/833897513349021706/955338658792243220/unsplash_Sm7ebvMgi-E_1.png",
+        "Product name", "Store name", "200"));
+    entries.add(Product(1, "https://cdn.discordapp.com/attachments/833897513349021706/955338900002455642/unsplash_2hpiy9XuXC4.png",
+        "Product name", "Store name", "200"));
+    entries.add(Product(2, "https://cdn.discordapp.com/attachments/833897513349021706/955339290785742858/unsplash_FiQNJA-CND4.png",
+        "Product name", "Store name", "200"));
+    entries.add(Product(3, "https://cdn.discordapp.com/attachments/833897513349021706/955338658792243220/unsplash_Sm7ebvMgi-E_1.png",
+        "Product name", "Store name", "200"));
     super.initState();
   }
 
@@ -43,7 +43,7 @@ class _page_storeState extends State<page_store> {
           _tittleText("Our Produts"),
           Expanded(
               child: ListView.builder(
-                  padding: const EdgeInsets.all(8),
+                  padding: const EdgeInsets.only(left: 25, right: 25),
                   itemCount: entries.length,
                   itemBuilder: (context, index) {
                     return _cardProduct(entries[index]);
@@ -60,57 +60,79 @@ class _page_storeState extends State<page_store> {
     );
   }
 
-  Widget _filterCard(text, icon, color) {
+  Widget _filterCard(String text, IconData icon, Color color) {
     return Container(
       color: color,
       child: IconButton(onPressed: null, icon: Icon(icon)),
     );
   }
 
-  Widget _cardProduct(product) {
-    return Card(
-      margin: const EdgeInsets.all(4.0),
-      color: Color(0xFF8E6FD8),
-      child: SizedBox(
-          width: 300,
-          height: 150,
-          child:
-              Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround, 
+  Widget _cardProduct(Product product) {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 20),
+      child:Container(
+      width: 328,
+      height: 132,
+      padding: const EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: const Color(0xffE2E2EC)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _cardImage(product.image),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-            ClipRRect(
-                  borderRadius: BorderRadius.horizontal(),
-                  child: Image(
-                    width: 150,
-                    height: 300,
-                    image: NetworkImage(
-                        product.image),
-                  ),
-                ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              
-              children: [
-                Text(product.name,
-                style: 
-                const TextStyle(
-                  fontWeight: FontWeight.w800,
-                  fontSize: 30
-                  )),
-                Text(
-                product.storeName, 
-                style: const TextStyle(
-                fontWeight: FontWeight.w100,
-                fontSize: 20)),
-                Text("${product.price} USD", 
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20
-                  ),)
-              ],
-            ),
-            Column()
-          ])),
+              _cardTitleText(product.name),
+              _cardSubtitleText(product.storeName),
+              _cardPriceText(product.price)
+            ],
+          )
+        ],
+      ),
+    ));
+  }
+
+  Widget _cardImage(String link) {
+    // Figma Flutter Generator Unsplashsm7ebvmgieWidget - RECTANGLE
+    return Container(
+        width: 110,
+        height: 200,
+        decoration: BoxDecoration(
+          image: DecorationImage(image: NetworkImage(link),fit: BoxFit.fitWidth),
+          borderRadius: BorderRadius.circular(10)
+        ));
+  }
+
+  Widget _cardTitleText(String text) {
+    return Text(text,
+        style: 
+        const TextStyle(
+        color: Color.fromRGBO(56, 53, 88, 1),
+        fontSize: 24,
+        fontWeight: FontWeight.normal,
+      ), textAlign: TextAlign.left,);
+  }
+
+  Widget _cardSubtitleText(String text) {
+    return Text(text,
+        style: const 
+        TextStyle(
+        color: Color.fromRGBO(127, 119, 198, 1),    
+        fontSize: 20,
+        fontWeight: FontWeight.normal,
+      ),textAlign: TextAlign.left);
+  }
+
+  Widget _cardPriceText(String text) {
+    return Text(
+      "$text USD",
+      style: const  TextStyle(
+        color: Color.fromRGBO(127, 119, 198, 1),
+        fontSize: 24,
+        fontWeight: FontWeight.normal,
+      ), textAlign: TextAlign.left
     );
   }
 }
