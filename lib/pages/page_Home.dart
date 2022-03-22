a426c1ba9268d5c74d106422eec6030c0c91fe9d
ahmedzaghlout <zaghlouta@uninorte.edu.co>
2022-03-21 17:50:44 -1100

    feat: Creating home page

--- lib/pages/page_Home.dart
+++ lib/pages/page_Home.dart
@@ -25,32 +25,87 @@ class Pagehomestate extends State<PageHome> {
       body: SafeArea(
         child: Column(
           children: [
-            Text(
+            const Text(
               "Hello, Pedro!",
               overflow: TextOverflow.ellipsis,
               textAlign: TextAlign.left,
               style: TextStyle(
                 color: Color.fromRGBO(30, 23, 33, 1),
-                fontSize: 48,
+                fontSize: 30,
                 fontFamily: 'Roboto',
                 fontWeight: FontWeight.w700,
               ),
             ),
-            Column(
-              children: [
-                _cardImage(
-                    'https://media.discordapp.net/attachments/955549239801446473/955557314943914024/events-smart-card.png'),
-                _texto(entrie.length.toString())    
-              ],
+            Expanded(
+                flex: 5,
+                child: Container(
+                  decoration: BoxDecoration(color: const Color(0xffE2E2EC)),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          _cardImage(
+                              'https://media.discordapp.net/attachments/955549239801446473/955557314943914024/events-smart-card.png'),
+                          Column(
+                            children: [
+                              _carddescripcion('Agenda'),
+                              _carddescripcioneventos(entrie.length.toString())
+                            ],
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          _cardhora('12:30'),
+                          _carddescripcion('Vacinee 1')
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          _cardhora('16:30'),
+                          _carddescripcion('Training section')
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          _cardhora('17:20'),
+                          _carddescripcion('Spa day')
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          _cardhora('18:00'),
+                          _carddescripcion('Walk with Antonella')
+                        ],
+                      ),
+                    ],
+                  ),
+                )),
+            Container(
+              width: 50,
+              height: 50,
             ),
             Expanded(
-                flex: 1,
-                child: ListView.builder(
-                    padding: const EdgeInsets.only(left: 25, right: 25),
-                    itemCount: entrie.length,
-                    itemBuilder: (context, index) {
-                      return _card(entrie[index]);
-                    })),
+                flex: 5,
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(color: const Color(0xffE2E2EC)),
+                  child: Column(
+                    children: [
+                      _cardImage2(
+                          'https://media.discordapp.net/attachments/955549239801446473/955561331161976922/unsplash_OA9103M2gSs.png'),
+                      _carddescripcioneventos(
+                          'Don’t forget to walk with your pets.'),
+                      _carddescripcion(
+                          'Dog owners enjoy numerous health and social benefits by walking their dog a few times a week. Benefits include improved cardiovascular fitness, lower blood pressure more...')
+                    ],
+                  ),
+                ))
           ],
         ),
       ),
@@ -88,7 +143,7 @@ class Pagehomestate extends State<PageHome> {
       hora,
       style: const TextStyle(
         color: Color.fromRGBO(246, 166, 65, 1),
-        fontSize: 12,
+        fontSize: 16,
         fontWeight: FontWeight.normal,
       ),
       textAlign: TextAlign.left,
@@ -100,28 +155,39 @@ class Pagehomestate extends State<PageHome> {
       descripcion,
       style: const TextStyle(
         color: Color.fromRGBO(0, 0, 0, 1),
-        fontSize: 12,
+        fontSize: 16,
         fontWeight: FontWeight.normal,
       ),
-      textAlign: TextAlign.right,
+      textAlign: TextAlign.center,
     );
   }
 
   Widget _cardImage(String link) {
     return Container(
-        width: 210,
-        height: 300,
+        width: 100,
+        height: 100,
         decoration: BoxDecoration(
             image: DecorationImage(
                 image: NetworkImage(link), fit: BoxFit.fitWidth),
             borderRadius: BorderRadius.circular(10)));
   }
+    Widget _cardImage2(String link) {
+    return Container(
+        width: 400,
+        height: 100,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(link), fit: BoxFit.fitWidth),
+            borderRadius: BorderRadius.circular(10)));
+  }
+
+
   Widget _carddescripcioneventos(String descripcion) {
-    return Text('$descripcion events'
-       ,
+    return Text(
+      '$descripcion events',
       style: const TextStyle(
         color: Color.fromRGBO(0, 0, 0, 1),
-        fontSize: 12,
+        fontSize: 34,
         fontWeight: FontWeight.normal,
       ),
       textAlign: TextAlign.right,
