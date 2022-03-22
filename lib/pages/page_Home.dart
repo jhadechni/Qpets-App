66b99d1eca22dfa6aa6954854324b8d5b7f88235
fabrixiop26 <fabriciopuche@hotmail.com>
2022-03-21 19:46:39 -0500

    feat: added basic searchbar

--- lib/pages/page_Home.dart
+++ lib/pages/page_Home.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-class page_Home extends StatelessWidget {
+import 'package:qpets_app/shared/search_bar.dart';
 
+class page_Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Home Page'),
+      child: SearchBar((s) => {print(s)}, "Search an Event"),
     );
   }
 }
