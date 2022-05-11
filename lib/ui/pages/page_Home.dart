8c2c2f30f9b7298f0ae72ca583e3fcdb49c0b73b
Jaime Sierra <49314853+jhadechni@users.noreply.github.com>
2022-05-11 00:49:11 -0500

    fix: fixing naming and firebase data obtaining for home page

--- lib/ui/pages/page_Home.dart
+++ lib/ui/pages/page_Home.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:qpets_app/domain/agenda.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../controllers/authentication_controller.dart';
+import 'package:qpets_app/controllers/user_controller.dart';
+import 'package:qpets_app/domain/authentication.dart';
+import 'package:qpets_app/domain/user.dart';
 
 class PageHome extends StatefulWidget {
   const PageHome({Key? key}) : super(key: key);
@@ -22,189 +27,206 @@ class Pagehomestate extends State<PageHome> {
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(30),
-                child: Text(
-                  "Hello, Pedro!",
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    color: Color.fromRGBO(30, 23, 33, 1),
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Container(
-                width: 339,
-                height: 308,
-                decoration: BoxDecoration(color: Color(0xffE2E2EC),borderRadius: BorderRadius.all(Radius.circular(10)), ),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        _cardImage(
-                            'https://media.discordapp.net/attachments/955549239801446473/955557314943914024/events-smart-card.png'),
-                        Column(
-                          children: [
-                            _carddescripcion('Agenda', true),
-                            _carddescripcioneventos(entrie.length.toString())
-                          ],
-                        )
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        _cardhora('12:30'),
-                        _carddescripcion('Vacinee 1', true)
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        _cardhora('16:30'),
-                        _carddescripcion('Training section', true)
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        _cardhora('17:20'),
-                        _carddescripcion('Spa day', true)
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        _cardhora('18:00'),
-                        _carddescripcion('Walk with Antonella', true)
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(30),
-                child: Container(
-                  decoration:  BoxDecoration(
-                    borderRadius: BorderRadius.circular(10), color: Color(0xffE2E2EC),),
+    UserController userController = Get.find();
+    AuthenticationController authentication = Get.find();
+    Authentication controller = Get.find();
+    return FutureBuilder<User>(
+        future: userController.fetchUserData(authentication.getUid()),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return  SafeArea(
+                    child: SingleChildScrollView(
                   child: Column(
                     children: [
-                      _cardImage2(
-                          'https://media.discordapp.net/attachments/955549239801446473/955561331161976922/unsplash_OA9103M2gSs.png'),
-                      _titulo('Don’t forget to walk with your pets.'),
-                      _carddescripcion(
-                          'Dog owners enjoy numerous health and social benefits by walking their dog a few times a week. Benefits include improved cardiovascular fitness, lower blood pressure more...', false)
+                      Padding(
+                        padding: const EdgeInsets.all(30),
+                        child: Text(
+                          "Hello, ${snapshot.data!.name}!",
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                            color: Color.fromRGBO(30, 23, 33, 1),
+                            fontSize: 48,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 339,
+                        height: 308,
+                        decoration: const BoxDecoration(
+                          color: const Color(0xffE2E2EC),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10)),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                _cardImage(
+                                    'https://media.discordapp.net/attachments/955549239801446473/955557314943914024/events-smart-card.png'),
+                                Column(
+                                  children: [
+                                    _carddescripcion('Agenda', true),
+                                    _carddescripcioneventos(
+                                        entrie.length.toString())
+                                  ],
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                _cardhora('12:30'),
+                                _carddescripcion('Vacinee 1', true)
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                _cardhora('16:30'),
+                                _carddescripcion('Training section', true)
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                _cardhora('17:20'),
+                                _carddescripcion('Spa day', true)
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                _cardhora('18:00'),
+                                _carddescripcion('Walk with Antonella', true)
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(30),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: const Color(0xffE2E2EC),
+                          ),
+                          child: Column(
+                            children: [
+                              _cardImage2(
+                                  'https://media.discordapp.net/attachments/955549239801446473/955561331161976922/unsplash_OA9103M2gSs.png'),
+                              _titulo('Don’t forget to walk with your pets.'),
+                              _carddescripcion(
+                                  'Dog owners enjoy numerous health and social benefits by walking their dog a few times a week. Benefits include improved cardiovascular fitness, lower blood pressure more...',
+                                  false)
+                            ],
+                          ),
+                        ),
+                      )
                     ],
                   ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+                ));
+          } else if (snapshot.hasError) {
+            return const Center(child: const Text("Error"));
+          } else {
+            return const Center(child: Text("Loading..."));
+          }
+        });
   }
+}
 
-  Widget _card(agenda agenda) {
-    return Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: const Color(0xffE2E2EC)),
-        padding: const EdgeInsets.only(bottom: 20),
-        child: Container(
-          width: 20,
-          height: 100,
-          padding:
-              const EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  _cardhora(agenda.hora),
-                  _carddescripcion(agenda.descripcion,true)
-                ],
-              )
-            ],
-          ),
-        ));
-  }
-
-  Widget _cardhora(String hora) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 10, left: 30,top: 10),
-      child: Text(
-        hora,
-        style: const TextStyle(
-          color: Color.fromRGBO(246, 166, 65, 1),
-          fontSize: 24,
-          fontWeight: FontWeight.normal,
-        ),
-        textAlign: TextAlign.left,
-      ),
-    );
-  }
-
-  Widget _carddescripcion(String descripcion, bool big) {
-    return Padding(
-      padding: big ? EdgeInsets.only(left: 30):  EdgeInsets.all(30),
-      child: Text(
-        descripcion,
-        style:  TextStyle(
-          color: Color.fromRGBO(0, 0, 0, 1),
-          fontSize: big ? 18 : 15,
-          fontWeight: FontWeight.normal,
-        ),
-      ),
-    );
-  }
-
-  Widget _cardImage(String link) {
-    return Container(
-        width: 100,
+Widget _card(agenda agenda) {
+  return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: const Color(0xffE2E2EC)),
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Container(
+        width: 20,
         height: 100,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: NetworkImage(link), fit: BoxFit.fitWidth),
-            borderRadius: BorderRadius.circular(10)));
-  }
+        padding:
+            const EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _cardhora(agenda.hora),
+                _carddescripcion(agenda.descripcion, true)
+              ],
+            )
+          ],
+        ),
+      ));
+}
 
-  Widget _cardImage2(String link) {
-    return Container(
-        width: 400,
-        height: 80,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: NetworkImage(link), fit: BoxFit.fitWidth),
-            borderRadius: BorderRadius.circular(10)));
-  }
-
-  Widget _carddescripcioneventos(String descripcion) {
-    return Text(
-      '$descripcion events',
+Widget _cardhora(String hora) {
+  return Padding(
+    padding: const EdgeInsets.only(bottom: 10, left: 30, top: 10),
+    child: Text(
+      hora,
       style: const TextStyle(
-        color: Color.fromRGBO(0, 0, 0, 1),
+        color: Color.fromRGBO(246, 166, 65, 1),
         fontSize: 24,
         fontWeight: FontWeight.normal,
       ),
-      textAlign: TextAlign.right,
-    );
-  }
+      textAlign: TextAlign.left,
+    ),
+  );
+}
 
-  Widget _titulo(String descripcion) {
-    return Text(
+Widget _carddescripcion(String descripcion, bool big) {
+  return Padding(
+    padding: big ? const EdgeInsets.only(left: 30) : const EdgeInsets.all(30),
+    child: Text(
       descripcion,
-      style: const TextStyle(
-        color: Color.fromRGBO(0, 0, 0, 1),
-        fontSize: 34,
+      style: TextStyle(
+        color: const Color.fromRGBO(0, 0, 0, 1),
+        fontSize: big ? 18 : 15,
         fontWeight: FontWeight.normal,
       ),
-      textAlign: TextAlign.center,
-    );
-  }
+    ),
+  );
+}
+
+Widget _cardImage(String link) {
+  return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+          image:
+              DecorationImage(image: NetworkImage(link), fit: BoxFit.fitWidth),
+          borderRadius: BorderRadius.circular(10)));
+}
+
+Widget _cardImage2(String link) {
+  return Container(
+      width: 400,
+      height: 80,
+      decoration: BoxDecoration(
+          image:
+              DecorationImage(image: NetworkImage(link), fit: BoxFit.fitWidth),
+          borderRadius: BorderRadius.circular(10)));
+}
+
+Widget _carddescripcioneventos(String descripcion) {
+  return Text(
+    '$descripcion events',
+    style: const TextStyle(
+      color: Color.fromRGBO(0, 0, 0, 1),
+      fontSize: 24,
+      fontWeight: FontWeight.normal,
+    ),
+    textAlign: TextAlign.right,
+  );
+}
+
+Widget _titulo(String descripcion) {
+  return Text(
+    descripcion,
+    style: const TextStyle(
+      color: Color.fromRGBO(0, 0, 0, 1),
+      fontSize: 34,
+      fontWeight: FontWeight.normal,
+    ),
+    textAlign: TextAlign.center,
+  );
 }
