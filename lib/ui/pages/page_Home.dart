4fe26dd5f3d2d037446525bba25a58bdf7d398a3
David Solano <solanoed@uninorte.edu.co>
2022-05-24 00:59:19 -0500

    fix:working HomeScreen

--- lib/ui/pages/page_Home.dart
+++ lib/ui/pages/page_Home.dart
@@ -20,12 +20,14 @@ class Pagehomestate extends State<PageHome> {
   EventController controller = Get.find<EventController>();
   List<agenda> entrie = <agenda>[];
   List<tipList> tips = <tipList>[];
+
   @override
   void initState() {
-    entrie.add(agenda('12:30', 'Vacinee 1'));
-    entrie.add(agenda('16:30', 'Training section'));
-    entrie.add(agenda('17:20', 'Spa day'));
-    entrie.add(agenda('18:00', 'Walk with Antonella'));
+    for (var i = 0; i < controller.events.length; i++) {
+      if (i < 4) {
+        entrie.add(agenda('12:30', controller.events[i].title));
+      }
+    }
     addList();
     super.initState();
   }
@@ -49,13 +51,14 @@ class Pagehomestate extends State<PageHome> {
         'tip_4'));
   }
 
+  EventController eventController = Get.find<EventController>();
   @override
   Widget build(BuildContext context) {
     UserController userController = Get.find();
     AuthenticationController authentication = Get.find();
-    Authentication controller = Get.find();
     Random random = Random();
     int rndm = random.nextInt(4);
+    Authentication controller = Get.find();
     return FutureBuilder<User>(
         future: userController.fetchUserData(authentication.getUid()),
         builder: (context, snapshot) {
@@ -98,30 +101,11 @@ class Pagehomestate extends State<PageHome> {
                             )
                           ],
                         ),
-                        Row(
-                          children: [
-                            _cardhora('12:30'),
-                            _carddescripcion('Vacinee 1', true)
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            _cardhora('16:30'),
-                            _carddescripcion('Training section', true)
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            _cardhora('17:20'),
-                            _carddescripcion('Spa day', true)
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            _cardhora('18:00'),
-                            _carddescripcion('Walk with Antonella', true)
-                          ],
-                        ),
+                        if (entrie.length < 4) ...[
+                          for (var i = 0; i < entrie.length; i++) ...[
+                            rowAgenda(i)
+                          ]
+                        ],
                       ],
                     ),
                   ),
@@ -151,106 +135,115 @@ class Pagehomestate extends State<PageHome> {
           }
         });
   }
-}
 
-Widget _card(agenda agenda) {
-  return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: const Color(0xffE2E2EC)),
-      padding: const EdgeInsets.only(bottom: 20),
-      child: Container(
-        width: 20,
-        height: 100,
-        padding:
-            const EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _cardhora(agenda.hora),
-                _carddescripcion(agenda.descripcion, true)
-              ],
-            )
-          ],
+  Widget rowAgenda(int i) {
+    return Row(
+      children: [
+        _cardhora(entrie[i].hora),
+        _carddescripcion(entrie[i].descripcion, true)
+      ],
+    );
+  }
+
+  Widget _card(agenda agenda) {
+    return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: const Color(0xffE2E2EC)),
+        padding: const EdgeInsets.only(bottom: 20),
+        child: Container(
+          width: 20,
+          height: 100,
+          padding:
+              const EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _cardhora(agenda.hora),
+                  _carddescripcion(agenda.descripcion, true)
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+
+  Widget _cardhora(String hora) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10, left: 30, top: 10),
+      child: Text(
+        hora,
+        style: const TextStyle(
+          color: Color.fromRGBO(246, 166, 65, 1),
+          fontSize: 24,
+          fontWeight: FontWeight.normal,
         ),
-      ));
-}
+        textAlign: TextAlign.left,
+      ),
+    );
+  }
 
-Widget _cardhora(String hora) {
-  return Padding(
-    padding: const EdgeInsets.only(bottom: 10, left: 30, top: 10),
-    child: Text(
-      hora,
+  Widget _carddescripcion(String descripcion, bool big) {
+    return Padding(
+      padding: big ? const EdgeInsets.only(left: 30) : const EdgeInsets.all(30),
+      child: Text(
+        descripcion,
+        style: TextStyle(
+          color: const Color.fromRGBO(0, 0, 0, 1),
+          fontSize: big ? 18 : 15,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+    );
+  }
+
+  Widget _cardImage() {
+    return Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage('assets/images/tip_1.png'),
+            ),
+            borderRadius: BorderRadius.circular(10)));
+  }
+
+  Widget _cardImage2(String img) {
+    return Container(
+        width: 400,
+        height: 80,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/images/$img.png'),
+            ),
+            borderRadius: BorderRadius.circular(10)));
+  }
+
+  Widget _carddescripcioneventos(String descripcion) {
+    return Text(
+      '$descripcion events',
       style: const TextStyle(
-        color: Color.fromRGBO(246, 166, 65, 1),
+        color: Color.fromRGBO(0, 0, 0, 1),
         fontSize: 24,
         fontWeight: FontWeight.normal,
       ),
-      textAlign: TextAlign.left,
-    ),
-  );
-}
+      textAlign: TextAlign.right,
+    );
+  }
 
-Widget _carddescripcion(String descripcion, bool big) {
-  return Padding(
-    padding: big ? const EdgeInsets.only(left: 30) : const EdgeInsets.all(30),
-    child: Text(
+  Widget _titulo(String descripcion) {
+    return Text(
       descripcion,
-      style: TextStyle(
-        color: const Color.fromRGBO(0, 0, 0, 1),
-        fontSize: big ? 18 : 15,
+      style: const TextStyle(
+        color: Color.fromRGBO(0, 0, 0, 1),
+        fontSize: 34,
         fontWeight: FontWeight.normal,
       ),
-    ),
-  );
-}
-
-Widget _cardImage() {
-  return Container(
-      width: 100,
-      height: 100,
-      decoration: BoxDecoration(
-          image: const DecorationImage(
-            image: AssetImage('assets/images/tip_1.png'),
-          ),
-          borderRadius: BorderRadius.circular(10)));
-}
-
-Widget _cardImage2(String img) {
-  return Container(
-      width: 400,
-      height: 80,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage('assets/images/$img.png'),
-          ),
-          borderRadius: BorderRadius.circular(10)));
-}
-
-Widget _carddescripcioneventos(String descripcion) {
-  return Text(
-    '$descripcion events',
-    style: const TextStyle(
-      color: Color.fromRGBO(0, 0, 0, 1),
-      fontSize: 24,
-      fontWeight: FontWeight.normal,
-    ),
-    textAlign: TextAlign.right,
-  );
-}
-
-Widget _titulo(String descripcion) {
-  return Text(
-    descripcion,
-    style: const TextStyle(
-      color: Color.fromRGBO(0, 0, 0, 1),
-      fontSize: 34,
-      fontWeight: FontWeight.normal,
-    ),
-    textAlign: TextAlign.center,
-  );
+      textAlign: TextAlign.center,
+    );
+  }
 }
